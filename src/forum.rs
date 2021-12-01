b3cdee207b3faf7138d3c2c28b0e7262eba666a9
Joshua Moon <josh@jaw.sh>
2021-12-01 19:24:09 +0100

    Merged in new database connection style.

--- src/forum.rs
+++ src/forum.rs
@@ -3,7 +3,7 @@ use askama_actix::Template;
 use chrono::prelude::Utc;
 use diesel::prelude::*;
 use ruforo::models::{NewPost, NewThread, NewUgcRevision, Post, Thread, UgcRevision};
-use ruforo::DbPool;
+use ruforo::MyAppData;
 use serde::Deserialize;
 
 #[derive(Template)]
@@ -21,62 +21,74 @@ pub struct NewThreadFormData {
 
 #[post("/forums/post-thread")]
 pub async fn create_thread(
-    __pool: web::Data<DbPool>,
-    __form: web::Form<NewThreadFormData>,
+    data: web::Data<MyAppData<'static>>,
+    form: web::Form<NewThreadFormData>,
 ) -> Result<HttpResponse, Error> {
     use crate::ugc::create_ugc;
     use diesel::insert_into;
     use ruforo::schema::posts::dsl::*;
     use ruforo::schema::threads::dsl::*;
 
-    let _conn = __pool.get().expect("couldn't get db connection from pool");
+    let conn = data
+        .pool
+        .get()
+        .expect("couldn't get db connection from pool");
 
     // Step 1. Create the UGC.
-    let _ugc: UgcRevision = create_ugc(
-        __pool,
+    let ugc: UgcRevision = create_ugc(
+        &conn,
         NewUgcRevision {
             ip_id: None,
             user_id: None,
-            content: Some((&__form.content).to_owned()),
+            content: Some((&form.content).to_owned()),
         },
     )
     .expect("couldn't create ugc for new thread");
 
     // Step 2. Create a thread.
-    let _thread: Thread = insert_into(threads)
+    let thread: Thread = insert_into(threads)
         .values(NewThread {
             user_id: None,
             created_at: Utc::now().naive_utc(),
-            title: __form.title.to_owned(),
-            subtitle: __form.subtitle.to_owned(),
+            title: form.title.to_owned(),
+            subtitle: form.subtitle.to_owned(),
         })
-        .get_result::<Thread>(&_conn)
+        .get_result::<Thread>(&conn)
         .expect("couldn't insert thread");
 
     // Step 3. Create a post with the correct associations.
-    let _post: Post = insert_into(posts)
+    insert_into(posts)
         .values(NewPost {
-            thread_id: _thread.id,
-            ugc_id: _ugc.id,
+            thread_id: thread.id,
+            ugc_id: ugc.id,
             user_id: None,
             created_at: Utc::now().naive_utc(),
         })
-        .get_result::<Post>(&_conn)
+        .get_result::<Post>(&conn)
         .expect("couldn't insert post");
 
     Ok(HttpResponse::Found()
-        .append_header(("Location", format!("/threads/{}/", _thread.id)))
+        .append_header(("Location", format!("/threads/{}/", thread.id)))
         .finish())
 }
 
 #[get("/forums/")]
-pub async fn read_forum(__pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
+pub async fn read_forum(data: web::Data<MyAppData<'static>>) -> Result<HttpResponse, Error> {
     use ruforo::schema::threads::dsl::*;
 
-    let _conn = __pool.get().expect("couldn't get db connection from pool");
-    let _threads: Vec<Thread> = threads
-        .get_results::<Thread>(&_conn)
+    let conn = data
+        .pool
+        .get()
+        .expect("couldn't get db connection from pool");
+    let our_threads: Vec<Thread> = threads
+        .get_results::<Thread>(&conn)
         .expect("error fetching ugc");
 
-    Ok(HttpResponse::Ok().body(ForumTemplate { threads: _threads }.render().unwrap()))
+    Ok(HttpResponse::Ok().body(
+        ForumTemplate {
+            threads: our_threads,
+        }
+        .render()
+        .unwrap(),
+    ))
 }
--- src/thread.rs
+++ src/thread.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web, Error, HttpResponse};
 use askama_actix::Template;
 use diesel::prelude::*;
 use ruforo::models::{NewUgcRevision, Thread, Ugc, UgcRevision};
-use ruforo::DbPool;
+use ruforo::MyAppData;
 use serde::Deserialize;
 
 #[derive(Template)]
@@ -19,51 +19,58 @@ pub struct NewPostFormData {
 
 #[post("/threads/{thread_id}/post-reply")]
 pub async fn create_reply(
-    __path: web::Path<(i32,)>,
-    __pool: web::Data<DbPool>,
-    __form: web::Form<NewPostFormData>,
+    data: web::Data<MyAppData<'static>>,
+    path: web::Path<(i32,)>,
+    form: web::Form<NewPostFormData>,
 ) -> Result<HttpResponse, Error> {
     use crate::ugc::create_ugc;
 
-    let _conn = __pool.get().expect("couldn't get db connection from pool");
+    let conn = data
+        .pool
+        .get()
+        .expect("couldn't get db connection from pool");
 
     create_ugc(
-        __pool,
+        &conn,
         NewUgcRevision {
             ip_id: None,
             user_id: None,
-            content: Some((&__form.content).to_owned()),
+            content: Some((&form.content).to_owned()),
         },
     )
     .expect("unable to insert new ugc");
 
     Ok(HttpResponse::Found()
-        .append_header(("Location", format!("/threads/{}/", __path.into_inner().0)))
+        .append_header(("Location", format!("/threads/{}/", path.into_inner().0)))
         .finish())
 }
 
 #[get("/threads/{thread_id}/")]
 pub async fn read_thread(
-    __path: web::Path<(i32,)>,
-    __pool: web::Data<DbPool>,
+    path: web::Path<(i32,)>,
+    data: web::Data<MyAppData<'static>>,
 ) -> Result<HttpResponse, Error> {
     use ruforo::schema::threads::dsl::*;
     use ruforo::schema::ugc::dsl::*;
 
-    let _conn = __pool.get().expect("couldn't get db connection from pool");
-    let _thread: Thread = threads
-        .find(__path.into_inner().0)
-        .get_result::<Thread>(&_conn)
+    let conn = data
+        .pool
+        .get()
+        .expect("couldn't get db connection from pool");
+
+    let our_thread: Thread = threads
+        .find(path.into_inner().0)
+        .get_result::<Thread>(&conn)
         .expect("error fetching thread");
-    let _ugc: Vec<Ugc> = ugc.get_results::<Ugc>(&_conn).expect("error fetching ugc");
-    let _ugc_revision: Vec<UgcRevision> = UgcRevision::belonging_to(&_ugc)
-        .load::<UgcRevision>(&_conn)
+    let our_ugc: Vec<Ugc> = ugc.get_results::<Ugc>(&conn).expect("error fetching ugc");
+    let our_ugc_revision: Vec<UgcRevision> = UgcRevision::belonging_to(&our_ugc)
+        .load::<UgcRevision>(&conn)
         .expect("error fetching ugc revisions");
 
     Ok(HttpResponse::Ok().body(
         ThreadTemplate {
-            thread: _thread,
-            posts: _ugc_revision,
+            thread: our_thread,
+            posts: our_ugc_revision,
         }
         .render()
         .unwrap(),
