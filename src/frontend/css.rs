534f24b0fe4b0904400ce0bcef0018a02c3c78ec
CrunkLord420 <crunklord420@lolcow.email>
2021-12-15 02:17:03 +0000

    optimized CSS generation

--- src/frontend/css.rs
+++ src/frontend/css.rs
@@ -1,19 +1,20 @@
 use actix_web::{get, Error, HttpResponse};
 use rsass::{compile_scss_path, output};
 
-// TODO
-// What we will want eventually is to precompile the CSS at `cargo run` and move it to /public/,
-// and allow CSS editing without restarting the application. For now, this works. As the CSS
-// becomes more complex this solution will be less acceptable.
+lazy_static! {
+    static ref CSS_CACHE: Vec<u8> = {
+        let path = "templates/css/main.scss".as_ref();
+        let format = output::Format {
+            style: output::Style::Compressed,
+            ..Default::default()
+        };
+        compile_scss_path(path, format).unwrap()
+    };
+}
+
 #[get("/style.css")]
 pub async fn view_css() -> Result<HttpResponse, Error> {
-    let path = "templates/css/main.scss".as_ref();
-    let format = output::Format {
-        style: output::Style::Compressed,
-        ..Default::default()
-    };
-
     Ok(HttpResponse::Ok()
         .content_type("text/css")
-        .body(compile_scss_path(path, format).unwrap()))
+        .body(CSS_CACHE.as_slice()))
 }
