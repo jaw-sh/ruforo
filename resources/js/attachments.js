66080c5a305fe26c67a762b54e0d750a4a50dbd2
Joshua Moon <josh@jaw.sh>
2022-01-13 17:56:33 +0100

    Client-side Blake3 hashing.

--- resources/js/attachments.js
+++ resources/js/attachments.js
@@ -5,7 +5,24 @@ import { blake3 } from 'hash-wasm';
 })();
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.attachment-upload').addEventListener('click', async function uploadFile(event) {
+    document.querySelector('.attachment-input').addEventListener('change', async function (event) {
+        let file = event.target.files[0];
+        if (file) {
+            let reader = new FileReader();
+
+            reader.onload = async function (readerEvent) {
+                let hash = await blake3(new Uint8Array(readerEvent.target.result));
+            }
+
+            reader.onerror = function (readerEvent) {
+                console.log("error reading file");
+            }
+
+            reader.readAsArrayBuffer(file);
+        }
+    });
+
+    document.querySelector('.attachment-upload').addEventListener('click', async function (event) {
         event.preventDefault();
 
         let formData = new FormData();
