cd9e2b17e4eb891fcca1851cc72f42eb99676e7f
Joshua Moon <josh@jaw.sh>
2022-05-21 09:12:29 +0200

    Entities generated for new tables

--- src/orm/prelude.rs
+++ src/orm/prelude.rs
@@ -2,7 +2,14 @@
 
 pub use super::attachment_thumbnails::Entity as AttachmentThumbnails;
 pub use super::attachments::Entity as Attachments;
+pub use super::forum_permissions::Entity as ForumPermissions;
+pub use super::forums::Entity as Forums;
+pub use super::groups::Entity as Groups;
 pub use super::ip::Entity as Ip;
+pub use super::permission_categories::Entity as PermissionCategories;
+pub use super::permission_collections::Entity as PermissionCollections;
+pub use super::permission_values::Entity as PermissionValues;
+pub use super::permissions::Entity as Permissions;
 pub use super::posts::Entity as Posts;
 pub use super::sessions::Entity as Sessions;
 pub use super::threads::Entity as Threads;
@@ -11,5 +18,6 @@ pub use super::ugc_attachments::Entity as UgcAttachments;
 pub use super::ugc_deletions::Entity as UgcDeletions;
 pub use super::ugc_revisions::Entity as UgcRevisions;
 pub use super::user_2fa::Entity as User2fa;
+pub use super::user_groups::Entity as UserGroups;
 pub use super::user_name_history::Entity as UserNameHistory;
 pub use super::users::Entity as Users;
