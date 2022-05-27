7280227dde170b950119c34c834e508a3c28be6d
Joshua Moon <josh@jaw.sh>
2022-05-27 15:53:09 +0200

    Re-added asset route

--- src/web/asset.rs
+++ src/web/asset.rs
@@ -1,8 +1,10 @@
+use actix_files as fs;
 use actix_web::http::{header, header::ContentEncoding, StatusCode};
-use actix_web::{get, HttpRequest, HttpResponse, Responder};
+use actix_web::{get, Error, HttpRequest, HttpResponse, Responder};
+use std::path::PathBuf;
 
 pub(super) fn configure(conf: &mut actix_web::web::ServiceConfig) {
-    conf.service(view_file_by_hash);
+    conf.service(view_file_by_hash).service(view_public_file);
 }
 
 /// Route for passing local assets through the webserver.
@@ -79,4 +81,16 @@ async fn view_file_by_hash(req: HttpRequest) -> impl Responder {
     builder.append_header(("Cache-Control", "public, max-age=31536000"));
 
     builder.streaming(body)
+}
+
+/// Dynamically access public files through the webserver.
+#[get("/public/assets/{filename:.*}")]
+async fn view_public_file(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let mut path: PathBuf = PathBuf::from("public/assets/");
+    let req_path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    path.push(req_path);
+
+    let file = fs::NamedFile::open(path)?;
+
+    Ok(file.use_last_modified(true))
 }
