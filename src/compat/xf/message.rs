8c9123a5687c1d1adc0fa05726b7bcaafe314f49
Joshua Moon <josh@jaw.sh>
2022-06-07 18:23:17 +0200

    Chat remove redis message history

--- src/compat/xf/message.rs
+++ src/compat/xf/message.rs
@@ -46,37 +46,6 @@ pub async fn insert_chat_message(
     .await
     .expect("Failed to insert chat_messagemessage into XF database.");
 
-    // add to redis store for old chat
-    let history_key = format!("xf[hb.chat.room{}.messages][1]", message.room_id);
-    let history_entry: redis::RedisResult<String> = redis.get(&history_key).await;
-
-    let mut message_history = match history_entry {
-        Ok(message_ids) => match serde_php::from_bytes::<VecDeque<u32>>(message_ids.as_bytes()) {
-            Ok(deser) => deser,
-            Err(err) => {
-                log::warn!("FAILED to deserialize {:?}", err);
-                Default::default()
-            }
-        },
-        Err(err) => {
-            log::warn!("FAILED to pull from redis {:?}", err);
-            Default::default()
-        }
-    };
-
-    message_history.extend([model.message_id]);
-
-    while message_history.len() > 20 {
-        message_history.pop_front();
-    }
-
-    let _: Result<(), RedisError> = redis
-        .set(
-            &history_key,
-            &serde_php::to_vec(&message_history).expect("Failed to serialize message history."),
-        )
-        .await;
-
     ClientMessage {
         id: message.id,
         room_id: message.room_id,
